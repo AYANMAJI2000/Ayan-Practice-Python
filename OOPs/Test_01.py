a1fc18784c8b6ac7471bb019939eac15abc20a63
Ayan Kumar Maji <majiayan812@gmail.com>
2021-12-12 20:35:36 +0530

    A sample python code on classes and object

--- OOPs/Test_01.py
+++ OOPs/Test_01.py
@@ -1,4 +1,26 @@
 '''
 My Very First program and commit
 '''
-print("Hello world")
+
+class Employee:
+
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+    def print(self):
+        print(f"Name is {self.name}")
+        print(f"Age is {self.age}")
+    
+    @staticmethod
+    def greet():
+        print("Good Morning")
+
+employee_1 = Employee("Ayan",21)
+employee_2 = Employee("Rahul",44)
+
+employee_1.print()
+employee_1.greet()
+employee_2.print()
+employee_2.greet()
+
